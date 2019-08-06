80c44ad9b61f513fd9eb546df30492f07718bd47
Diana Badas <diana.badas@gmail.com>
2019-08-06 14:31:01 +0000

    Automatically backed up by Learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -1,7 +1,12 @@
 class SongsWorker
+  require 'csv'
   include Sidekiq::Worker
 
   def perform(songs_file)
-
+    CSV.foreach(songs_file, headers: true) do |song|
+      Song.find_or_create_by(title: song[0])
+      Song.last.artist = Artist.find_or_create_by(name: song[1])
+    end
   end
+  
 end
